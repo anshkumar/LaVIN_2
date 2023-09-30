c3e112a1a4da83577dfaf58c37d03ba22967ad71
vedanshu <anshgravity@gmail.com>
2023-09-30 14:01:42 +0530

    run_demo.sh added

---
+++ scripts/run_demo.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node 1 --nnodes 1 --node_rank 0 --master_port 11111 /home/sort/ved/LaVIN_2/demo.py \
+    --llm_model 7B\
+    --max_seq_len 512 \
+    --ckpt_dir ../data/weights/ \
+    --adapter_type attn\
+    --adapter_path LaVIN-7B/checkpoint-19.pth
+    --temperature 10.\
